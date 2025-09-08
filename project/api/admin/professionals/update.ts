bf7270ac477eaef34f378737280b48d85cd5a3f0
masashitakano917-maker <masashitakano917@gmail.com>
2025-09-08 14:38:02 +0900

    Update update.ts

--- project/api/admin/professionals/update.ts
+++ project/api/admin/professionals/update.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getSupabaseAdmin } from '../../_supabaseAdmin';
+import { getSupabaseAdmin } from '../../_supabaseAdmin.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Cache-Control', 'no-store');
